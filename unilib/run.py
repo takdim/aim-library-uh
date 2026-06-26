097f21cf0c2daca9479e7fca94b6520ebd8d9e6f
takdim <takdim253@gmail.com>
2026-06-26 13:49:13 +0800

    fix card modal in home

--- unilib/run.py
+++ unilib/run.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app(os.environ.get('FLASK_ENV', 'development'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
